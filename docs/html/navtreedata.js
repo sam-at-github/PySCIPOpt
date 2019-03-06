938bec534a6d357b41c4bb6b1bf586ca81884381
Matthias Miltenberger <miltenberger@zib.de>
2019-03-06 16:02:45 +0100

    first docu commit

---
+++ docs/html/navtreedata.js
@@ -0,0 +1,34 @@
+var NAVTREE =
+[
+  [ "PySCIPOpt", "index.html", [
+    [ "Overview", "index.html", "index" ],
+    [ "PySCIPOpt", "md_README.html", null ],
+    [ "Requirements", "md_INSTALL.html", null ],
+    [ "Contributing to PySCIPOpt", "md_CONTRIBUTING.html", null ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classpyscipopt_1_1scip_1_1Model.html#a4c8d6961a72151e551f10e4c6fd4f915",
+"diet_8py_source.html",
+"lotsizing__lazy_8py.html",
+"scip_8pxd.html#a2169d26fa56db326fb5bcfa2e9a68101",
+"scip_8pyx.html#a92791f739f660cf8d21cf592f4bccb7c"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
